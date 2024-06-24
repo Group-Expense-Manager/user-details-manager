30c0f7c55cc7918c25806a25db5f66125028a9f3
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-06-24 14:59:14 +0200

    GEM-13 | cleanup

--- src/main/kotlin/pl/edu/agh/gem/internal/persistence/ProductRepository.kt
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.gem.internal.persistence
-
-import pl.edu.agh.gem.internal.model.Product
-
-interface ProductRepository {
-    fun find(id: String): Product?
-
-    fun save(product: Product)
-}
