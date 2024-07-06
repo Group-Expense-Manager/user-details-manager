1f0188d2e4977f38174b9e2c0a839c916ea698da
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-07-06 09:28:56 +0200

    GEM-40 | fixes

--- src/main/kotlin/pl/edu/agh/gem/external/dto/validation/username/NullOrUsernamePatternValidator.kt
+++ src/main/kotlin/pl/edu/agh/gem/external/dto/validation/username/NullOrUsernamePatternValidator.kt
@@ -5,7 +5,7 @@ import jakarta.validation.ConstraintValidatorContext
 import java.util.regex.Pattern
 
 class NullOrUsernamePatternValidator : ConstraintValidator<NullOrUsernamePattern?, String?> {
-    private val pattern: Pattern = Pattern.compile("^[a-zA-Z0-9_-]{3,20}$")
+    private val pattern: Pattern = Pattern.compile("^[a-zA-Z0-9_.+-]{3,20}$")
     override fun isValid(value: String?, constraintValidatorContext: ConstraintValidatorContext): Boolean {
         return value == null || pattern.matcher(value).matches()
     }
